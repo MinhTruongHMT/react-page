1d272f2178ddfc5110ab23599afb7fadba0f213d
MinhTruongHMT <ninjarua2118@gmail.com>
2023-05-26 15:43:42 +0700

    them togle button

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -34,13 +34,9 @@ const StyleSideBar = styled.div`
     .switch {
       position: relative;
       display: inline-block;
-      width: 56px;
+      width: 50px;
       height: 30px;
-      input {
-        opacity: 0;
-        width: 0;
-        height: 0;
-      }
+      margin: 0 10px;
       .slider {
         position: absolute;
         cursor: pointer;
@@ -49,43 +45,29 @@ const StyleSideBar = styled.div`
         right: 0;
         bottom: 0;
         background-color: #ccc;
-        -webkit-transition: 0.4s;
         transition: 0.4s;
-      }
-
-      .slider:before {
-        position: absolute;
-        content: "";
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
-        background-color: white;
-        -webkit-transition: 0.4s;
-        transition: 0.4s;
-      }
-
-      input:checked + .slider {
-        background-color: #2196f3;
-      }
-
-      input:focus + .slider {
-        box-shadow: 0 0 1px #2196f3;
-      }
-
-      input:checked + .slider:before {
-        -webkit-transform: translateX(26px);
-        -ms-transform: translateX(26px);
-        transform: translateX(26px);
-      }
-
-      /* Rounded sliders */
-      .slider.round {
         border-radius: 34px;
       }
-
-      .slider.round:before {
-        border-radius: 50%;
+      input {
+        display: none;
+      }
+      .slider::before {
+        position: absolute;
+        content: "";
+        height: 25px;
+        width: 25px;
+        top: 2px;
+        left: 5px;
+        right: 5px;
+        background-color: white;
+        transition: 0.4s;
+        border-radius: 50px;
+      }
+      input:checked + .slider {
+        background-color: #5429FF;
+      }
+      input:checked + .slider::before {
+        transform: translateX(17px);
       }
     }
   }
@@ -119,7 +101,7 @@ const NavItem = ({ path, text, image }) => {
 export const SideBar = () => {
   return (
     <StyleSideBar>
-      <Logo/>
+      <Logo />
       <NavItem text="Dashboard" path="/" image={Dashboard}></NavItem>
       <NavItem text="Market" path="/about" image={Shop}></NavItem>
       <NavItem text="Login" path="/login" image={Judge}></NavItem>
@@ -132,12 +114,14 @@ export const SideBar = () => {
       <div className="TitleSideBar">Other</div>
       <div className="Toggle">
         <NavItem text="Light Mode" path="/login" image={Light}></NavItem>
-        <label class="switch">
-          <input type="checkbox" checked />
-          <span class="slider round"></span>
+        {/* <i class="fa-regular fa-sun"></i>
+          <i class="fa-solid fa-moon"></i> */}
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider"></span>
         </label>
       </div>
-      <Card/>
+      <Card />
     </StyleSideBar>
   );
 };
